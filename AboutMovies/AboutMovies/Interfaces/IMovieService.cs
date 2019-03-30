1e226eae81bfd208f5428f15c8323fe90e943c08
Gustavo Pereira de Freitas <meia.gustavo@gmail.com>
2019-03-30 14:15:08 -0300

    Added interfaces IMovieService and IRequestError

---
+++ AboutMovies/AboutMovies/Interfaces/IMovieService.cs
@@ -0,0 +1,9 @@
+﻿using AboutMovies.Model;
+using System.Threading.Tasks;
+
+namespace AboutMovies.Interfaces {
+    public  interface IUpcomingMovieService
+    {
+        Task<UpcomingMovie> GetUpcomingMoviesAsync();
+    }
+}
---
+++ AboutMovies/AboutMovies/Interfaces/IRequestError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AboutMovies.Interfaces
+{
+    public interface IRequestError
+    {
+        bool Error { get; set; }
+
+        string MessageErro { get; set; }
+
+        int StatusCode { get; set; }
+    }
+}
