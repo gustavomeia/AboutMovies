eb3ca23ea2d2cb5399d8aaca68e71ad75686bb0c
Gustavo Pereira de Freitas <meia.gustavo@gmail.com>
2019-03-30 14:14:04 -0300

    Added UpcomingMovie and Movie Models

---
+++ AboutMovies/AboutMovies/Model/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AboutMovies.Model
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string PosterUrl { get; set; }
+    }
+}
---
+++ AboutMovies/AboutMovies/Model/UpcomingMovie.cs
@@ -0,0 +1,21 @@
+﻿using AboutMovies.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace AboutMovies.Model
+{
+    public class UpcomingMovie : IRequestError
+    {
+        public List<Movie> Movies { get; set; } = new List<Movie>();
+
+        public int Page { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public bool Error { get; set; }
+
+        public string MessageErro { get; set; }
+
+        public int StatusCode { get; set; }
+    }
+}
