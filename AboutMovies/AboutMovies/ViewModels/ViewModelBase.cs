3b051e1f95dd183ba02e97f5c2466eab7d129cf3
Gustavo Pereira de Freitas <meia.gustavo@gmail.com>
2019-03-30 16:16:37 -0300

    Added some properties ont ViewModelBase

--- AboutMovies/AboutMovies/ViewModels/ViewModelBase.cs
+++ AboutMovies/AboutMovies/ViewModels/ViewModelBase.cs
@@ -1,41 +1,57 @@
 ﻿using Prism.Mvvm;
 using Prism.Navigation;
 
-namespace AboutMovies.ViewModels
-{
-    public class ViewModelBase : BindableBase, INavigationAware, IDestructible
-    {
+namespace AboutMovies.ViewModels {
+    public class ViewModelBase : BindableBase, INavigationAware, IDestructible {
         protected INavigationService NavigationService { get; private set; }
 
         private string _title;
-        public string Title
-        {
+        public string Title {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
-        public ViewModelBase(INavigationService navigationService)
-        {
+        private bool _isBusy;
+        public bool IsBusy {
+            get => _isBusy;
+            set {
+                if (SetProperty(ref _isBusy, value))
+                    IsNotBusy = !_isBusy;
+            }
+        }
+
+        private bool _isNotBusy = true;
+        public bool IsNotBusy {
+            get => _isNotBusy;
+            set {
+                if (SetProperty(ref _isNotBusy, value))
+                    IsBusy = !_isNotBusy;
+            }
+        }
+
+        private bool _canLoadMore = true;
+        public bool CanLoadMore {
+            get => _canLoadMore;
+            set => SetProperty(ref _canLoadMore, value);
+        }
+
+        public ViewModelBase(INavigationService navigationService) {
             NavigationService = navigationService;
         }
 
-        public virtual void OnNavigatedFrom(INavigationParameters parameters)
-        {
+        public virtual void OnNavigatedFrom(INavigationParameters parameters) {
 
         }
 
-        public virtual void OnNavigatedTo(INavigationParameters parameters)
-        {
+        public virtual void OnNavigatedTo(INavigationParameters parameters) {
 
         }
 
-        public virtual void OnNavigatingTo(INavigationParameters parameters)
-        {
+        public virtual void OnNavigatingTo(INavigationParameters parameters) {
 
         }
 
-        public virtual void Destroy()
-        {
+        public virtual void Destroy() {
 
         }
     }
